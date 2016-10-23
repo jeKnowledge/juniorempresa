ec172b9e39c1447acf4bf42ca01ff9df2fb7bb99
João Botelho <joao.botelho@jeknowledge.com>
2016-10-23 19:51:19 +0100

    Adds vertical horizontal dots

--- scripts/jsfullpage_func.js
+++ scripts/jsfullpage_func.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
     anchors: ['Main_Page','Junior_1', 'Junior_2', 'Junior_3','Informations'],
     sectionsColor: ['#F9F9F9', '#F9F9F9', '#F9F9F9', '#F9F9F9','#F9F9F9'],
     menu: '#menu',
+    navigation: true,
+		navigationPosition: 'left',
+		navigationTooltips: ['Introdu&ccedil&atildeo','Junior 1', 'Junior 2', 'Junior 3','Informa&ccedil&otildees'],
     slidesNavigation: true,
     ccs3: true,
     scrollingSpeed: 600
   });
+
+  $("#fp-nav").find("ul li").first().remove();
 });
