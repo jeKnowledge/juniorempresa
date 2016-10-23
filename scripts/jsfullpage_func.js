0dbfb95e05c3680cbdf6c9f41ad28f575be4a794
João Botelho <joao.botelho@jeknowledge.com>
2016-10-23 01:32:34 +0100

    adds collors and anchors to last 2 sections

--- scripts/jsfullpage_func.js
+++ scripts/jsfullpage_func.js
@@ -1,6 +1,8 @@
-$('#fullpage').fullpage({
-  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
-  anchors: ['junior1', 'junior2', 'junior3'],
-  menu: '#menu',
-  scrollingSpeed: 1000
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE','#E27D7D','#E27D7D'],
+    anchors: ['Main_Page','Junior_1', 'Junior_2', 'Junior_3','Informations'],
+    menu: '#menu',
+    scrollingSpeed: 1000
+  });
 });
