de02c4432af662bf12ea6a88aafb06cdbd8d97f1
João Botelho <joao.botelho@jeknowledge.com>
2016-10-23 02:27:48 +0100

    removes last color for last section

--- scripts/jsfullpage_func.js
+++ scripts/jsfullpage_func.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE','#E27D7D','#E27D7D'],
+    sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE','#E27D7D'],
     anchors: ['Main_Page','Junior_1', 'Junior_2', 'Junior_3','Informations'],
     menu: '#menu',
     scrollingSpeed: 1000
