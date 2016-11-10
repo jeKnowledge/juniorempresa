433178b60fd18ba4d2e12a298e04385ad2044fc0
João Botelho <joao.botelho@jeknowledge.com>
2016-11-10 19:50:28 +0000

    new main page with new text and images, removes some sections and adds a new comp. section

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,25 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['Main_Page','Junior_1', 'Junior_2', 'Junior_3','Informations'],
-    sectionsColor: ['#F9F9F9', '#F9F9F9', '#F9F9F9', '#F9F9F9','#F9F9F9'],
+    sectionsColor: ['#F9F9F9', '#F9F9F9', '#376092', '#F9F9F9','#F9F9F9'],
     menu: '#menu',
-    navigation: true,
-		navigationPosition: 'left',
-		navigationTooltips: ['Introdu&ccedil&atildeo','Junior 1', 'Junior 2', 'Junior 3','Informa&ccedil&otildees'],
+    continuousHorizontal: true,
     slidesNavigation: true,
     ccs3: true,
     scrollingSpeed: 600,
     afterLoad: function(anchorLink, index) {
       switch (index) {
         case 2:
-          $(".je-image").attr('src','images/exemplo1.png');
+          $(".je-image").attr('src','images/jekrect.png');
           break;
         case 3:
-          $(".je-image").attr('src','images/exemplo2.png');
-          break;
-        case 4:
-          $(".je-image").attr('src','images/exemplo3.png');
+          $(".je-image").attr('src','images/logo_teste.png');
           break;
         default:
-          $(".je-image").attr('src','images/jek3.png');
+          $(".je-image").attr('src','');
+
           break;
       }
     }
   });
-  $("#fp-nav").find("ul li").first().remove();
 });
