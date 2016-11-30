f2a07d2de7194bf5ccade80c0412c7903755ae16
João Botelho <joao.botelho@jeknowledge.com>
2016-11-30 20:03:43 +0000

    changes slides apresentation

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,11 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors: ['Main_Page','Junior_1', 'Junior_2','Informations'],
-    sectionsColor: ['white', '#F9F9F9', '#376092','grey'],
+    sectionsColor: ['white', 'white', '#376092','grey'],
     menu: '#menu',
     continuousHorizontal: true,
     slidesNavigation: true,
     ccs3: true,
-    scrollingSpeed: 600,
-    afterLoad: function(anchorLink, index) {
-      switch (index) {
-        case 2:
-          $(".je-image").attr('src','images/jekrect.png');
-          break;
-        case 3:
-          $(".je-image").attr('src','images/logo_teste.png');
-          break;
-        default:
-          $(".je-image").attr('src','');
-
-          break;
-      }
-    }
+    scrollingSpeed: 600
   });
 });
